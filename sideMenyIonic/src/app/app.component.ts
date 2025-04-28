8a5bc342bca7ce5cb585f5ed1cf675e550bd4dee
Taylormeira <taylortmeira25@gmail.com>
2025-04-27 21:23:11 -0300

    create Branch

---
+++ sideMenyIonic/src/app/app.component.ts
@@ -0,0 +1,25 @@
+
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { barbellSharp, bookmarkSharp, calculatorSharp, homeSharp, speedometerSharp } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/folder/inbox', icon: 'home' },
+    { title: 'Área do Triângulo', url: 'velocidamedia', icon: 'speedometer' },
+    { title: 'Área do Círculo', url: 'fatorial', icon: 'calculator' },
+    { title: 'forca', url: '/folder/archived', icon: 'barbell' },
+  ];
+  public labels = ['Mattematica', 'Fisica'];
+  constructor() {
+    addIcons({ homeSharp, bookmarkSharp, speedometerSharp, barbellSharp, calculatorSharp });
+  }
+}
